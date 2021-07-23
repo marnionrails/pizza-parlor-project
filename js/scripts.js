147cf61ea7823c6f0c22cc2b320900e1336ffb27
marnionrails <37088724+marnionrails@users.noreply.github.com>
2021-07-22 21:13:06 -0400

    add prototype function for base cost

--- js/scripts.js
+++ js/scripts.js
@@ -4,3 +4,17 @@ function Pizza() {
   this.price = 0;
 }
 
+let pizza = new Pizza();
+console.log(pizza);
+
+Pizza.prototype.baseCost = function(size) {
+  this.size = size;
+  if (this.size === "8-inch") {
+      this.price = 6;
+  } else if (this.size === "12-inch") {
+      this.price = 8;
+  } else {
+    alert("Opps! Something went wrong!");
+  }
+  return this.price;
+};
